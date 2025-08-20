d2f65c4a3e06185cd97f2ddf656fefe8a09300b5
saeedbm <iamsaeedbm@gmail.com>
2025-08-20 22:58:32 +0330

    add drift setup

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_drift_crud/widgets/add_task.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: addTaskButton(),
-      body: SafeArea(child: Column(children: [Text('test')])),
+      body: SafeArea(child: Column(children: [Text('data')])),
     );
   }
 }
