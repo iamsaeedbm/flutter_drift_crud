204fea5bf1b35bbe13be5534050a8f395a5373b5
saeedbm <iamsaeedbm@gmail.com>
2025-08-31 22:43:04 +0330

    modify database

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,7 +8,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: addTaskButton(),
-      body: SafeArea(child: Column(children: [Text('test5')])),
+      body: SafeArea(child: Column(children: [Text('test6')])),
     );
   }
 }
